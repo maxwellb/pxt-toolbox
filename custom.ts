20b5a1132b4fafd3012245e36d751bb214a03697
Maxwell Bloch <maxwellb@users.noreply.github.com>
2021-02-02 03:21:09 -0500

    Add blink function

--- custom.ts
+++ custom.ts
@@ -12,4 +12,27 @@ namespace com.github.maxwellb.toolbox {
     export function noop(): void {
         // no operation
     }
+
+    //% block="Blink %pixeloffset|at a rate of %bpm|%repeat|times"
+    export function blink(pixeloffset: number, bpm: number, repeat: number): void {
+        const baseColor = light.pixelColor(pixeloffset);
+        const steps = [0, 10, 10, 10, 40, 80, 120, 180, 240];
+        const msPerBeat = 60000/bpm;
+        const msPerHalfBeat = msPerBeat/2;
+        const msPerStep = msPerHalfBeat/steps.length;
+        const dark = light.rgb(0,0,0);
+
+        light.setPixelColor(pixeloffset, dark);
+        for (let i = 0; i < repeat; i++) {
+            for (let j = 0; j < steps.length; j++) {
+                light.setPixelColor(pixeloffset, color.fade(baseColor, steps[j]));
+                basic.pause(msPerStep);
+            }
+            for (let j = 0; j < steps.length; j++) {
+                light.setPixelColor(pixeloffset, color.fade(baseColor, steps[steps.length-j-1]));
+                basic.pause(msPerStep);
+            }
+        }
+        light.setPixelColor(pixeloffset, baseColor);
+    }
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-forever(function () {
-    com.github.maxwellb.toolbox.noop()
-})
+com.github.maxwellb.toolbox.noop()
+pause(2000)
+light.setAll(0xff8000)
+pause(1000)
+com.github.maxwellb.toolbox.blink(1, 120, 10)
+pause(2000)
+light.setAll(0x000000)
+pause(1000)
