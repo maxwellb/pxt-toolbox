c07139c5c03a12aec86f87abed11a1ec337b1165
Maxwell Bloch <maxwellb@users.noreply.github.com>
2021-02-02 03:32:29 -0500

    Skip the test code for packaging

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-com.github.maxwellb.toolbox.noop()
-pause(2000)
-light.setAll(0xff8000)
-pause(1000)
-com.github.maxwellb.toolbox.blink(1, 120, 10)
-pause(2000)
-light.setAll(0x000000)
-pause(1000)
+// Set to true to test
+if (false) {
+    com.github.maxwellb.toolbox.noop()
+    pause(2000)
+    light.setAll(0xff8000)
+    pause(1000)
+    com.github.maxwellb.toolbox.blink(1, 120, 10)
+    pause(2000)
+    light.setAll(0x000000)
+    pause(1000)
+}
